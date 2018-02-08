ef621e2c1bbd1004c138676ec44c70df94de14ac
Daniel Cher <dchernovolenko@stuy.edu>
2018-02-08 13:44:57 -0500

    2/8 work

--- game.js
+++ game.js
@@ -2,8 +2,13 @@ var canvas = document.getElementById("thing");
 var clear = document.getElementById("clear");
 var stopv = document.getElementById("Stop");
 var ctx = canvas.getContext("2d");
+var r = 0;
 var t = 0;
 var z = 1;
+var x = Math.random()*500;
+var y = Math.random()*500;
+var dx = 5;
+var dy = 5;
 var requestID;
 
 ctx.beginPath();
@@ -11,24 +16,41 @@ ctx.beginPath();
 
 var drawcirc = function(){
     ctx.beginPath();
-    ctx.arc(250,250, t, 0, 2* Math.PI);
+    ctx.arc(250,250, r, 0, 2* Math.PI);
     clearcan()
     ctx.fill();
     ctx.stroke();
-    console.log(t)
-    if(t<250 && z == 1){
-        t++;
+    if(r<250 && z == 1){
+        r++;
     }
-    if(t == 250){
-        z = 0
+    if(r == 250){
+       z = 0
     }
     if(z == 0){
-        t--
+        r--
     }
-    if(t == 0){
+    if(r == 0){
         z = 1
     }
     requestID = window.requestAnimationFrame(drawcirc);
+
+}
+
+var screensaver = function(){
+    ctx.beginPath();
+    ctx.arc(x,y, 20, 0, 2* Math.PI);
+    clearcan()
+    ctx.fill();
+    ctx.stroke();
+    if(x>480 || x<20){
+        dx = -dx;
+    }
+    if(y>480 || y<20){
+       dy = -dy;
+    }
+    x += dx;
+    y += dy;
+    requestID = window.requestAnimationFrame(screensaver);
 
 }
 
@@ -38,12 +60,34 @@ var clearcan = function(){
 
 var clearfull = function(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    t = 0;
+    r = 0;
 }
 var stop = function(){
     window.cancelAnimationFrame (requestID)
 }
 
+var draw = function(){
+    if( t == 0) {
+        clearfull()
+        drawcirc();
+    }
+    else{
+        clearfull()
+       screensaver();
+    }
+}
+
+var togglefunc = function(){
+    if( t == 0){
+        t = 1;
+        console.log(t);
+    }
+    else {
+        t= 0;
+    }
+}
+
 stopv.addEventListener("click", stop);
 clear.addEventListener("click", clearfull);
-canvas.addEventListener("click", drawcirc);
+canvas.addEventListener("click", draw);
+toggle.addEventListener("click", togglefunc);
